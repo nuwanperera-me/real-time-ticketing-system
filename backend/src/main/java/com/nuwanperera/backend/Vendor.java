d8926c571a3c7a10159462207b36ec68974bf919
Nuwan Perera <nuwanperera613@gmail.com>
2024-11-23 23:50:09 +0530

    feat: add vendor class

---
+++ backend/src/main/java/com/nuwanperera/backend/Vendor.java
@@ -0,0 +1,52 @@
+package com.nuwanperera.backend;
+
+import java.util.Random;
+
+public class Vendor implements Runnable {
+  private final int id;
+  private int ticketPerRelease;
+  private int releaseInterval;
+
+  public Vendor(int ticketPerRelease, int releaseInterval) {
+    this.id = new Random().nextInt(100_000);
+    this.ticketPerRelease = ticketPerRelease;
+    this.releaseInterval = releaseInterval;
+  }
+
+  @Override
+  public void run() {
+    while (true) {
+      for (int i = 0; i < ticketPerRelease; i++) {
+        Ticket ticket = new Ticket();
+        System.out.printf("Vendor %d is adding ticket %d%n", id, ticket.getId());
+        TicketPool.getInstance().addTicket(ticket);
+      }
+      try {
+        Thread.sleep(releaseInterval);
+      } catch (InterruptedException e) {
+        Thread.currentThread().interrupt();
+        System.out.printf("Vendor %d interrupted.%n", id);
+      }
+    }
+  }
+
+  public int getVendorId() {
+    return id;
+  }
+
+  public int getTicketPerRelease() {
+    return ticketPerRelease;
+  }
+
+  public int setTicketPerRelease(int ticketPerRelease) {
+    return this.ticketPerRelease = ticketPerRelease;
+  }
+
+  public int getReleaseInterval() {
+    return releaseInterval;
+  }
+
+  public int setReleaseInterval(int releaseInterval) {
+    return this.releaseInterval = releaseInterval;
+  }
+}
