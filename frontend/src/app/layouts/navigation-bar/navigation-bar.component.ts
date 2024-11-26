f7426d577dc6c091df140c431e7325589af54b6b
Nuwan Perera <nuwanperera613@gmail.com>
2024-11-26 13:38:43 +0530

    feat: add navigation bar

--- frontend/src/app/layouts/navigation-bar/navigation-bar.component.ts
+++ frontend/src/app/layouts/navigation-bar/navigation-bar.component.ts
@@ -1,10 +1,25 @@
+import { NgIf, NgFor } from '@angular/common';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navigation-bar',
   standalone: true,
+  imports: [NgIf, NgFor],
   templateUrl: './navigation-bar.component.html',
 })
 export class NavigationBarComponent {
+  navigationLinks = [
+    { title: 'Vendors', link: '/vendors' },
+    { title: 'Customers', link: '/customers' },
+    { title: 'Settings', link: '/settings' },
+  ];
 
+  isRunning: boolean = true;
+
+  constructor(private router: Router) {}
+
+  routeIsActive(route: string): boolean {
+    return window.location.pathname === route;
+  }
 }
