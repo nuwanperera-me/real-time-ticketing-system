b0229a409f821da66f38c00c7c8c1a7c9b3b5d63
Nuwan Perera <nuwanperera613@gmail.com>
2024-11-26 17:07:04 +0530

    fix: cors error

--- backend/src/main/java/com/nuwanperera/backend/BackendApplication.java
+++ backend/src/main/java/com/nuwanperera/backend/BackendApplication.java
@@ -8,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class BackendApplication {
 	HashMap<Integer, Customer> customers = new HashMap<Integer, Customer>();
 	HashMap<Integer, Vendor> vendors = new HashMap<Integer, Vendor>();
