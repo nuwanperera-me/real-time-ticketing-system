fe961de395b4751d9f741eeda8ababee75aa2e7a
Nuwan Perera <nuwanperera613@gmail.com>
2024-12-10 14:24:47 +0530

    feat (frontend): add settings page for ticket system configuration

---
+++ frontend/src/app/pages/settings/settings.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import {
+  Configuration,
+  ConfigurationStoreService,
+} from '../../core/store/configuration-store.service';
+import { ConfigurationService } from '../../core/services/configuration.service';
+import { NgTemplateOutlet } from '@angular/common';
+
+@Component({
+  selector: 'app-settings',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './settings.component.html',
+})
+export class SettingsComponent implements OnInit {
+  config: Configuration = {
+    totalTickets: 0,
+    ticketReleaseRate: 0,
+    customerRetrievalRate: 0,
+    maxTicketsCapacity: 0,
+    runningStatus: false,
+  };
+
+  isEditing: boolean = false;
+
+  constructor(
+    private configStore: ConfigurationStoreService,
+    private configService: ConfigurationService
+  ) {}
+
+  ngOnInit(): void {
+    this.configStore.config$.subscribe((config) => {
+      this.config = config;
+    });
+
+    this.configService.stopPolling();
+  }
+
+  ngOnDestroy(): void {
+    this.configService.startPolling();
+  }
+
+  saveConfiguration() {
+    console.log(this.config);
+    this.configService.updateConfiguration(this.config);
+  }
+}
